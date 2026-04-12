e465bfbe1568ecce064032fee2a4453bf5396f38
Sebastian Strus <sebastian.strus1@gmail.com>
2026-04-12 19:56:04 +0200

    Initial Commit

---
+++ TarotGo/TarotGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarotGoApp.swift
+//  TarotGo
+//
+//  Created by Sebastian Strus on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct TarotGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
